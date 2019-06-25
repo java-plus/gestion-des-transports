a49ac2045bae891fbb223e321243eb2238b0a661
Olivier LASBLEIS <lasbleis.olivier@yahoo.fr>
2019-06-25 14:41:22 +0200

    nono

--- src/main/java/fr/diginamic/controller/collaborateur/SupprimerMonAnnonce.java
+++ src/main/java/fr/diginamic/controller/collaborateur/SupprimerMonAnnonce.java
@@ -1,5 +1,42 @@
 package fr.diginamic.controller.collaborateur;
 
-public class SupprimerMonAnnonce {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.diginamic.dao.CovoiturageDao;
+import fr.diginamic.dao.ResaCovoiturageDao;
+import fr.diginamic.model.Employe;
+
+@WebServlet(urlPatterns = "/controller/collaborateur/annulerAnnonce/*")
+public class SupprimerMonAnnonce extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		HttpSession session = req.getSession(false);
+
+		Employe utilisateurCourant = (Employe) session.getAttribute("utilisateur");
+
+		Integer idAnnonceCovoiturage = Integer.parseInt(req.getParameter("idAnnonce"));
+
+		Integer idUtilisateur = utilisateurCourant.getId();
+
+		ResaCovoiturageDao resaCovoiturageDao = new ResaCovoiturageDao();
+		resaCovoiturageDao.annulerToutesLesReservations(idAnnonceCovoiturage);
+
+		CovoiturageDao covoiturageDao = new CovoiturageDao();
+		covoiturageDao.annulerAnnonce(idAnnonceCovoiturage, idUtilisateur);
+
+		// Afficher les reservations via la liste listeDesReservations
+		// et java dans JSP
+
+		resp.sendRedirect("http://localhost:8080/gdt/controller/collaborateur/annonces");
+	}
 
 }
